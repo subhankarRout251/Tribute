fbcddae46df53122c64f04bf8785c9ff8d7c54e0
Subhankar Rout <118932561+subhankarRout251@users.noreply.github.com>
2023-04-28 22:15:25 +0530

    Add files via upload

---
+++ Tribute/assets/script.js
@@ -0,0 +1,34 @@
+// Add alert for user 
+// Add variables for Virgil's info
+// Console log info 
+
+alert("Welcome to my tribute page on Virgil Abloh for the believers")
+
+var feedback = confirm("If you wish to continue, click OK",)
+
+var appreciation = prompt("How has Virgil impacted your life?")
+
+alert(feedback)
+
+alert(appreciation) 
+
+// Occupations
+var timeline = "1980-2021"
+console.log("Virgil lived from " + timeline)
+
+var occupation_1 = "Civil engineer"
+var occupation_2 = "DJ"
+var occupation_3 = "Designer"
+var occupation_4 = "Architect"
+
+console.log("Virgil Abloh was a: " + '\n' + '--------------------' + '\n' + occupation_1 + '\n' + occupation_2 + '\n' + occupation_3 + '\n' + occupation_4)
+
+// Collabrations
+var collab_1 = "Nike"
+var collab_2 = "Ikea"
+var collab_3 = "Donda"
+var collab_4 = "Gore-tex"
+var collab_5 = "Nigo"
+var collab_6 = "Evian"
+
+console.log("His collabrations included: " + '\n' + '----' + '\n' + collab_1 + '\n' + collab_2 + '\n' + collab_3 + '\n' + collab_4 + '\n' + collab_5 + '\n' + collab_6 ) 
